2dc551f40860bb2646d3d78d3b4434f235df83ca
DmitryBelo <Bdsxx@yandex.ru>
2023-07-05 10:24:31 +0300

    Create Practical_work-1-3.py

---
+++ Practical_work-1-3.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#задание 1
+
+'''if условие | (условие):
+    действие 
+    .....
+    .....'''
+    
+# Задание 2
+'''
+pogoda = input('Какая сегодня погода? ')
+temp = int(input('Какая температура сегодня? '))
+
+if pogoda == 'дождь':
+    print('Возьмите зонт...')
+if pogoda == 'солнце':
+    print('Возьмите кепку...')
+    if temp > 30:
+        print("Намажтесь кремом от солнца...")
+
+
+# Задание 3
+
+is_bool = False
+
+if !is_bool:
+    print(1)
+else:
+    print(0)
+    
+# Задание 4
+x = 8
+y = 10
+if x < 15:
+    print('Меньшею...')
+elif x > 15:
+    print('Больше...')
+else:
+    print('Ровно...')'''
+
+
+#Задание 5
+x = 3
+y = 5
+
+if x > 5 or y < 8:
+    print(True)
